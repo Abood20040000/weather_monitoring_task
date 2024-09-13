9a5f965436a3bd6fd9ef90e0c464b7e0bc1f52be
Abood20040000 <111181745+Abood20040000@users.noreply.github.com>
2024-09-13 15:24:30 +0300

    Update remote_station_service.py

--- remote_station_service.py
+++ remote_station_service.py
@@ -9,17 +9,29 @@ class RemoteStationService(Node):
         self.servive_=self.create_service(
             MyData,"Remote_Station_Service",self.callback
         )
+        self.temp_state=True
+        self.press_state=True
+        self.humidity_state=True
     
     def callback(self,request,response):
         if not (10.0<=request.temp<=100.0):
             self.get_logger().info(f"The Temp:{request.temp} is out of range")
+            self.temp_state=False
         if not (0.7<=request.humidity<=0.95):
             self.get_logger().info(f"The humidity:{request.humidity} is out of range")
+            self.press_state=False
         if not (0.95<=request.pressure<=1.2):
-            self.get_logger().info(f"The pressure:{request.pressure} is out of range")   
+            self.get_logger().info(f"The pressure:{request.pressure} is out of range")  
+            self.humidity_state=False 
 
-        self.get_logger().info(f"the data :temp={request.temp},humidity={request.humidity},pressure={request.pressure}")  
-        response.response="all the data are valid"
+        if(self.temp_state==True and self.press_state==True and self.humidity_state==True):
+            self.get_logger().info(f"the data after ckeck :temp={request.temp},humidity={request.humidity},pressure={request.pressure}")  
+            response.response="all the data are valid"
+
+        else:
+            response.response="Try again , invalid data"
+
+
         
         return response     
 
